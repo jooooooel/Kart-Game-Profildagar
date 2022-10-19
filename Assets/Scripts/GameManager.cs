80b51c46c6143808ed0861f0a1a22e37b047fe11
jooooooel <90244606+jooooooel@users.noreply.github.com>
2022-10-19 15:48:28 +0200

    Got a little bit of progress
    Started developing a system for calculating the distance traveled, distance yet to be traveled and total distance yet to be traveled.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,21 +6,38 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    ArcadeKart playerKart;
+    public ArcadeKart playerKart;
 
     [SerializeField]
-    List<Transform> levelCheckpoints;
+    public List<Transform> levelCheckpoints;
+    [SerializeField]
+    public List<Transform> finishLine;
+
+    public float distanceToNextCheckpoint;
+    public float totalDistance;
 
     void Start()
     {
         if(!playerKart)
         {
-            playerKart = FindObjectOfType<ArcadeKart>();
+             playerKart = FindObjectOfType<ArcadeKart>();
+            
         }
     }
 
-    void Update()
+    public void Update()
     {
-        
+        CheckpointChecker();
     }
+    public void CheckpointChecker()
+    {
+        for (int i = 0; i < i; i++)
+        {
+            
+        }
+        Vector3 CheckpointB = levelCheckpoints[0].transform.position;
+        distanceToNextCheckpoint = Mathf.Sqrt(Mathf.Pow(CheckpointB.x - playerKart.transform.position.x, 2) + Mathf.Pow(CheckpointB.y - playerKart.transform.position.y, 2) + Mathf.Pow(CheckpointB.z - playerKart.transform.position.z, 2));
+    }
+
+
 }
